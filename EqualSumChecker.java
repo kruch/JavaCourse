55e9c0ae7e20dbadd5a1d4eaf40515bd03e798d1
Adrian Kruszynski <adrian.kruszynski@gmail.com>
2018-07-21 18:25:59 +0200

    - adding solution to EqualSumChecker

---
+++ EqualSumChecker.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class EqualSumChecker {
+
+    public static void main(String[] args) {
+    }
+
+    public static boolean hasEqualSum(int a, int b, int c)
+    {
+        if(a+b == c) return true;
+        return false;
+    }
+
+}
