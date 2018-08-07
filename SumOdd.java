e47b1bd85e0f78ae4a9a8b368c173a82fc35c982
Adrian Kruszynski <adrian.kruszynski@gmail.com>
2018-08-07 18:36:55 +0200

    - adding solution for loop exercise sumOdd

---
+++ SumOdd.java
@@ -0,0 +1,36 @@
+package com.company;
+
+
+
+public class SumOdd {
+    public static void main(String[] args) {
+
+        System.out.println(sumOdd(1, 100));
+        System.out.println(sumOdd(-1, 100));
+        System.out.println(sumOdd(100, 100));
+        System.out.println(sumOdd(100, -100));
+        System.out.println(sumOdd(100, 1000));
+
+    }
+
+    public static boolean isOdd(int number)
+    {
+        if(number < 0) return false;
+        if(number % 2 != 0) return true;
+        return false;
+    }
+
+    public static int sumOdd(int start, int end)
+    {
+        int sum = 0;
+        boolean odd;
+        if((start<=0 || end<=0) || end < start) return -1;
+        for(int i = start ; i <= end ; i++)
+        {
+            odd = isOdd(i);
+            if(odd) sum+=i;
+        }
+        return sum;
+    }
+ }
+
